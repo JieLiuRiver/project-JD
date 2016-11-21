6184dc6daa51a0bfedc3a9bee66ae2f2d1a06b65
stupidWall <268079@163.com>
2016-11-21 18:24:40 +0800

    update file

--- base/js/app.js
+++ base/js/app.js
@@ -1,146 +1,162 @@
-Vue.component('jd-header', {
-    template : '#jd-header-template'
-});
-Vue.component('jd-search', {
-    template : '#jd-search-template'
-});
-Vue.component('jd-grid', {
-    template : '#jd-grid-template'
-});
-Vue.component('jd-seckill', {
-    template : '#jd-seckill-template'
-});
-Vue.component('jd-fbt', {
-    template : '#jd-fbt-template'
-});
-Vue.component('jd-fbt', {
-    template : '#jd-fbt-template'
-});
-Vue.component('jd-coupon', {
-    template : '#jd-coupon-template'
-});
-Vue.component('jd-rec1', {
-    template : '#jd-rec1-template'
-});
-Vue.component('jd-lives', {
-    template : '#jd-lives-template'
-});
-Vue.component('jd-portal', {
-    template : '#jd-portal-template'
-});
-Vue.component('jd-entry', {
-    template : '#jd-entry-template'
-});
-Vue.component('jd-spec', {
-    template : '#jd-spec-template'
-});
+(function(){
+    'use strict';
+    Vue.component('jd-header', {
+        template : '#jd-header-template',
+        data: function(){
+            var configData = {
+                provinceData : null
+            };
+            /*获取省数据*/
+            $.post('base/data/province.json', function(resp){
+                configData.provinceData = resp;
+            });
+            return configData;
+        },
+        methods : {
 
-Vue.component('jd-more', {
-    template : '#jd-more-template'
-});
+        }
+    });
+    Vue.component('jd-search', {
+        template : '#jd-search-template'
+    });
+    Vue.component('jd-grid', {
+        template : '#jd-grid-template'
+    });
+    Vue.component('jd-seckill', {
+        template : '#jd-seckill-template'
+    });
+    Vue.component('jd-fbt', {
+        template : '#jd-fbt-template'
+    });
+    Vue.component('jd-fbt', {
+        template : '#jd-fbt-template'
+    });
+    Vue.component('jd-coupon', {
+        template : '#jd-coupon-template'
+    });
+    Vue.component('jd-rec1', {
+        template : '#jd-rec1-template'
+    });
+    Vue.component('jd-lives', {
+        template : '#jd-lives-template'
+    });
+    Vue.component('jd-portal', {
+        template : '#jd-portal-template'
+    });
+    Vue.component('jd-entry', {
+        template : '#jd-entry-template'
+    });
+    Vue.component('jd-spec', {
+        template : '#jd-spec-template'
+    });
 
-Vue.component('jd-service', {
-    template : '#jd-service-template'
-});
+    Vue.component('jd-more', {
+        template : '#jd-more-template'
+    });
 
-Vue.component('jd-help', {
-    template : '#jd-help-template'
-});
+    Vue.component('jd-service', {
+        template : '#jd-service-template'
+    });
 
-
-Vue.component('jd-copyright', {
-    template : '#jd-copyright-template'
-});
-
-
-var header = new Vue({
-    el : "#jd-header"
-});
-
-var serach = new Vue({
-    el : "#jd-search"
-});
-
-var grid = new Vue({
-    el : "#jd-grid"
-});
-
-var seckill = new Vue({
-    el : "#jd-seckill"
-});
-
-var fbt = new Vue({
-    el : "#jd-fbt"
-});
-
-var coupon = new Vue({
-    el : "#jd-coupon"
-});
-
-var rec1 = new Vue({
-    el : "#jd-rec1"
-});
-
-var entry1 = new Vue({
-    el : "#jd-entry1"
-});
-
-var rec2 = new Vue({
-    el : "#jd-rec2"
-});
+    Vue.component('jd-help', {
+        template : '#jd-help-template'
+    });
 
 
-var rec3 = new Vue({
-    el : "#jd-rec3"
-});
+    Vue.component('jd-copyright', {
+        template : '#jd-copyright-template'
+    });
 
-var rec4 = new Vue({
-    el : "#jd-rec4"
-});
 
-var lives = new Vue({
-    el : "#jd-lives"
-});
+    var header = new Vue({
+        el : "#jd-header"
+    });
 
-var coupon1 = new Vue({
-    el : "#jd-portal1"
-});
-var coupon2 = new Vue({
-    el : "#jd-portal2"
-});
-var coupon3 = new Vue({
-    el : "#jd-portal3"
-});
-var coupon4 = new Vue({
-    el : "#jd-portal4"
-});
-var coupon5 = new Vue({
-    el : "#jd-portal5"
-});
-var coupon6 = new Vue({
-    el : "#jd-portal6"
-});
-var coupon7 = new Vue({
-    el : "#jd-portal7"
-});
-var spec = new Vue({
-    el : "#jd-spec"
-});
+    var serach = new Vue({
+        el : "#jd-search"
+    });
 
-var more = new Vue({
-    el : "#jd-more"
-});
+    var grid = new Vue({
+        el : "#jd-grid"
+    });
 
-var service = new Vue({
-    el : "#jd-service"
-});
+    var seckill = new Vue({
+        el : "#jd-seckill"
+    });
 
-var help = new Vue({
-    el : "#jd-help"
-});
+    var fbt = new Vue({
+        el : "#jd-fbt"
+    });
 
-var copyright = new Vue({
-    el : "#jd-copyright"
-});
+    var coupon = new Vue({
+        el : "#jd-coupon"
+    });
 
+    var rec1 = new Vue({
+        el : "#jd-rec1"
+    });
+
+    var entry1 = new Vue({
+        el : "#jd-entry1"
+    });
+
+    var rec2 = new Vue({
+        el : "#jd-rec2"
+    });
+
+
+    var rec3 = new Vue({
+        el : "#jd-rec3"
+    });
+
+    var rec4 = new Vue({
+        el : "#jd-rec4"
+    });
+
+    var lives = new Vue({
+        el : "#jd-lives"
+    });
+
+    var coupon1 = new Vue({
+        el : "#jd-portal1"
+    });
+    var coupon2 = new Vue({
+        el : "#jd-portal2"
+    });
+    var coupon3 = new Vue({
+        el : "#jd-portal3"
+    });
+    var coupon4 = new Vue({
+        el : "#jd-portal4"
+    });
+    var coupon5 = new Vue({
+        el : "#jd-portal5"
+    });
+    var coupon6 = new Vue({
+        el : "#jd-portal6"
+    });
+    var coupon7 = new Vue({
+        el : "#jd-portal7"
+    });
+    var spec = new Vue({
+        el : "#jd-spec"
+    });
+
+    var more = new Vue({
+        el : "#jd-more"
+    });
+
+    var service = new Vue({
+        el : "#jd-service"
+    });
+
+    var help = new Vue({
+        el : "#jd-help"
+    });
+
+    var copyright = new Vue({
+        el : "#jd-copyright"
+    });
+
+})();
 
